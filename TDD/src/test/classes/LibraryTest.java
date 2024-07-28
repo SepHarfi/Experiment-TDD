deacaf260f2d6eb09d67b3a5d2aaba040e460a65
Mohammad Mahdi Vahedi <m.m.vahedi13800@gmail.com>
2024-07-28 21:06:47 +0330

    Add return book unit test

--- TDD/src/test/classes/LibraryTest.java
+++ TDD/src/test/classes/LibraryTest.java
@@ -4,6 +4,7 @@ import main.classes.*;
 import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class LibraryTest {
 
@@ -16,6 +17,21 @@ public class LibraryTest {
 
         Student student = new Student("Sepehr", 1);
         assertFalse(library.lendBook(book, student));
+    }
+
+    @Test
+    public void returnBook() {
+        Library library = new Library();
+
+        Book book = new Book("Book", "MMV", 1);
+        library.addBook(book);
+
+        Student student = new Student("Sepehr", 1);
+        library.addStudent(student);
+
+        assertTrue(library.lendBook(book, student));
+        assertTrue(library.returnBook(book, student));
+        assertFalse(student.hasBook(book));
     }
 
 }
